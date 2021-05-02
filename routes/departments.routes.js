c168a4031bda732b9218a193fb9aa5bc8db74ce5
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-05-02 14:09:44 +0200

    Change endpoints - after deleting or changing a document they returned it in response

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { update } = require('../models/department.model');
 const router = express.Router();
 const Department = require('../models/department.model');
 
@@ -55,18 +56,17 @@ router.post('/departments', async (req, res) => {
 });
 
 router.put('/departments/:id', async (req, res) => {
-  const { name } = req.body;
-
+  const { name: name } = req.body;
   try {
     const dep = await(Department.findById(req.params.id));
     if(dep) {
-      dep.name = name;
-      await dep.save();
-      res.json({ message: 'OK' });
-    }
-    else res.status(404).json({ message: 'Not found...' });
-  }
-  catch(err) {
+      await Department.updateOne({ _id: req.params.id }, { $set: { name: name } });
+      const updatedDep = await(Department.findById(req.params.id));
+      res.json({ message: 'OK' , updatedDep: updatedDep});
+    } 
+    else res.status(404).json({ message: 'Not found...' })    
+  } 
+  catch (err) {
     res.status(500).json({ message: err });
   }
 
@@ -78,7 +78,7 @@ router.delete('/departments/:id', async (req, res) => {
     const dep = await(Department.findById(req.params.id));
     if(dep) {
       await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK' });
+      res.json({ message: 'OK', deletedDep: dep});
     }
     else res.status(404).json({ message: 'Not found...' });
   }
